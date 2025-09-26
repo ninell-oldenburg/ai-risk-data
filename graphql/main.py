905d746c879f0255950bc269b36b282c5a50faef
Ninell Oldenburg <ninelloldenburg@gmail.com>
2025-09-26 13:05:46 +0200

    making it work!

--- graphql/main.py
+++ graphql/main.py
@@ -3,35 +3,34 @@ import src.json_to_csv as to_csv
 import src.extract_links_and_gender as lng
 import src.topic_clust as tc
 import src.topics_to_csv as ttc
-import src.arxiv_to_doi as atd
 import sys
 
 class Main:
     def __init__(self):
-        self.scrape() = scrape.main()
-        self.to_csv() = to_csv.main()
-        self.lng() = lng.main()
-        self.tc() = tc.main()
-        self.ttc() = ttc.main()
-        self.atd() = atd.main()
+        self.scrape = scrape.main
+        self.to_csv = to_csv.main
+        self.lng = lng.main
+        self.tc = tc.main
+        self.ttc = ttc.main
 
 def main(forum):
-    main = Main()
-    main.scrape(forum)
-    main.to_csv(forum)
-    main.lng(forum)
-    ntopics = main.tc(forum)
-    main.ttc(forum, ntopics)
-    main.atd()
+    processor = Main()
+    processor.scrape(forum)
+    processor.to_csv(forum)
+    processor.lng(forum)
+    ntopics = processor.tc(forum)
+    processor.ttc(forum, ntopics)
 
 if __name__ == '__main__':
-
-    if len(sys.argv) != 1:
+    # Check for exactly 2 arguments (script name + forum)
+    if len(sys.argv) != 2:
         print("==================================================")
-        print("==== USAGE: 'python graphiql.py/main.py FORUM ====")
+        print("==== USAGE: 'python main.py FORUM'            ====")
         print("==== [FORUM]: forum you want to download      ====")
         print("====          == lw  for lesswrong.com OR     ====")
         print("====          == af  for alignmentforum.org   ====")
         print("==================================================")
+        sys.exit(1)
     else:
-        main(sys.argv[0])
+        # Use the actual forum argument, not the script name
+        main(sys.argv[1])
